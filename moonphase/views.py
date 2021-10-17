e4cc8b098165c6b3f6a0b0f4c3f4d8907a5eec42
alv2017 <v.alishauskaite@gmail.com>
2021-10-17 13:34:54 +0300

    Application user interface has been improved.

--- moonphase/views.py
+++ moonphase/views.py
@@ -2,6 +2,7 @@ import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
+from django.contrib import messages
 from .moon_phase_calculator import get_lunar_phase
 
 
@@ -9,6 +10,8 @@ class MoonPhaseView(LoginRequiredMixin, View):
     def get(self, request):
         dt = datetime.date.today()
         moon_phase, moon_age = get_lunar_phase(dt)
+        messages.success(request, f'Hello {request.user} !')
+        messages.success(request, 'Welcome to our Space Station!')
         context = {
             'current_date': dt,
             'moon_phase': moon_phase,
